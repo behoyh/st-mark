c97d0531aa5514bda7c611adf76538a81d69b68e
Shaddy <sm1@rgbsi.com>
2016-06-22 13:22:18 -0400

    tweak home/parent component

--- client/app.js
+++ client/app.js
@@ -9,7 +9,7 @@
             $routeProvider
                 .when('/home', {
                     templateUrl: 'components/home/home.html',
-                    controller: 'HomeController'
+                    controller: 'HomeComponentController'
                 })
                 
                 .otherwise({ redirectTo: '/home' });
--- client/components/home/index.js
+++ client/components/home/index.js
@@ -1,3 +1,5 @@
-var app = angular.module('ClientBoilerplate');
-
-app.controller('HomeController', ['$scope', require('./home.controller')]);
+(function(angular){
+    angular.module('ClientBoilerplate')
+    .controller('HomeComponentController', ['$scope', require('./home-component.controller')])
+    .directive('clientBoilerplate', require('./home-component.directive'));
+})(window.angular); 
