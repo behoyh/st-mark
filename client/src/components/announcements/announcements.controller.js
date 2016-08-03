09cb43ac5e1cd91e5a7d547f259958883a4da18b
root <root@BEHOYSLATE.localdomain>
2016-08-03 12:06:42 -0400

    fixes yo

--- client/src/components/announcements/announcements.controller.js
+++ client/src/components/announcements/announcements.controller.js
@@ -9,17 +9,7 @@ function AnnouncementsController($location, $mdDialog){
     function notes(ev, guest){
         let confirm = $mdDialog.confirm()
           .title('Notes')
-          .textContent(`
-            No children under the age of 13 will be permitted in the retreat . Child care and activities are available for children ages 4 to 12 on Friday, Sept 2. Registration is required for childcare.
-
-            All events will be held at St. Mary & St. Philopateer Church and lectures will be given in Arabic
-
-            Live streaming will be available for all events for those who cannot attend
-
-            Pre-registration is required
-
-            A donation of $10 a person is suggested.
-          `)
+          .textContent(`No children under the age of 13 will be permitted in the retreat. Child care and activities are available for children ages 4 to 12 on Friday, Sept 2. Registration is required for childcare. All events will be held at St. Mary & St. Philopateer Church and lectures will be given in Arabic. Live streaming will be available for all events for those who cannot attend. Pre-registration is required. A donation of $10 a person is suggested.`)
           .ariaLabel('Notes')
           .targetEvent(ev)
           .ok('I understand')
--- client/src/components/landing.controller.js
+++ client/src/components/landing.controller.js
@@ -1,11 +1,11 @@
 module.exports = LandingController;
 
-LandingController.$inject = ['$rootScope', '$mdSidenav', '$log', '$timeout', '$location', 'userControlsService', 'authService', '$route'];
-function LandingController($rootScope, $mdSidenav, $log, $timeout, $location, userControlsService, authService, $route) {
+LandingController.$inject = ['$rootScope', '$mdSidenav', '$log', '$timeout', '$location', 'userControlsService', 'authService', '$route', '$mdDialog'];
+function LandingController($rootScope, $mdSidenav, $log, $timeout, $location, userControlsService, authService, $route, $mdDialog) {
     let vm = this;
-    
-    vm.brand = "St Mark";
-    vm.title;
+
+    vm.brand = "Fr. Daoud Lamei Spiritual Retreat";
+    vm.title = "" ;
     vm.close = close;
     vm.toggleLeft = buildDelayedToggler();
     vm.navigateTo = navigateTo;
@@ -49,14 +49,30 @@ function LandingController($rootScope, $mdSidenav, $log, $timeout, $location, us
     }
 
     function navigateTo(place) {
-        $log.info(`go to ${place}`);
-        $location.url(`/${place}`);
+        $location.url(place);
     }
 
     function logout(){
         userControlsService.logout();
         $rootScope.user = '';
         $route.reload();
+    }
+
+    vm.notes = notes;
+
+    function notes(ev, guest){
+        let confirm = $mdDialog.confirm()
+          .title('Notes')
+          .textContent('No children under the age of 13 will be permitted in the retreat. Child care and activities are available for children ages 4 to 12 on Friday, Sept 2. Registration is required for childcare. All events will be held at St. Mary & St. Philopateer Church and lectures will be given in Arabic. Live streaming will be available for all events for those who cannot attend. Pre-registration is required. A donation of $10 a person is suggested.')
+          .targetEvent(ev)
+          .ok('I understand')
+          .cancel('Cancel');
+
+        $mdDialog.show(confirm).then(function() {
+            $location.url('/event-form');
+        }, function() {
+
+        });
     }
 
 }
